3534a96b4224becd80e70f3d27754310cc616d3c
neerajnanvani <neerajnanvaniwork@gmail.com>
2024-01-30 22:26:49 +0530

    Loop day 1 done

---
+++ Js_week_3/script.js
@@ -0,0 +1,59 @@
+// console.log("Welcome to javascript");
+// console.log("Namaste");
+
+// console.log(y);
+
+// let x;
+
+// console.log(x);
+
+// let a = 1;
+
+// let b = 2;
+
+// let mul = a * b;
+
+// let str = "MY name is neeraj";
+// console.log(str);
+
+
+// let table = 5;
+
+// console.log(table, " X 1 = ", table * 1);
+
+// console.log(table, " X 2 = ", table * 2);
+
+// console.log(table, " X 3 = ", table * 3);
+
+// console.log(table, " X 4 = ", table * 4);
+
+// console.log(table, " X 5 = ", table * 5);
+
+// let condition = true;
+
+// while(condition) {
+//     console.log("Hey HYC BUDDIES");
+
+//     condition = false;
+// }
+
+
+// let num = 1;
+
+// while(num < 5) {
+//     console.log(num);
+
+//     num = num + 1;
+// }
+
+
+let table = 120;
+let count = 20;
+let count_start = 10;
+
+while(count_start <= count) {
+    console.log(table, " X ", count_start, " = ", table * count_start);
+    count_start = count_start + 1;
+}
+
+console.log("While loop ended");
