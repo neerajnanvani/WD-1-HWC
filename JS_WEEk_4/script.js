e84e72df058d1395c2678d93efa3b2e560857fd7
neerajnanvani <neerajnanvaniwork@gmail.com>
2024-02-05 22:11:04 +0530

    JS week 4 start

---
+++ JS_WEEk_4/script.js
@@ -0,0 +1,53 @@
+// let str = "My name is khan";
+
+// console.log(str.length);
+
+
+// console.log(str[5]);
+
+// console.log(str[13]);
+
+// console.log(str[15]);
+
+// let str1 = "hello";
+
+// let str2 = "Hello";
+
+// console.log(str1 == str2);
+
+// let str3 = "H";
+
+// console.log(str3 == str2[0]);
+
+
+// let index = 0;
+
+// let str = "Namaste Beta Namaste";
+
+// while(index < str.length) {
+//     console.log(str[index]);
+
+//     index++;
+// }
+
+
+
+let index = 0;
+
+let str = "bts";
+
+while(index < str.length) {
+
+    if(str[index] == 'a' || str[index] == 'e' || str[index] == 'i' || str[index] == 'o' || str[index] == 'u') {
+
+        console.log('isme ek vowel hai.');
+
+        index = str.length;
+    }
+
+    index++;
+}
+
+
+
+
