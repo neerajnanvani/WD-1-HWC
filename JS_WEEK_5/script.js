935dac3321f4ab01bbed17e869cbc878329569e8
neerajnanvani <neerajnanvaniwork@gmail.com>
2024-02-12 22:02:34 +0530

    JS weke 5 code

---
+++ JS_WEEK_5/script.js
@@ -0,0 +1,58 @@
+// console.log("HEY EVERYONE");
+
+// replace all characters from 'o' to 'e'
+
+// let str = "Neeraj";
+
+// // str = "prem";
+
+
+// console.log(str);
+
+// str[3];
+
+// let str = "p" + "r" + "e" + "m";
+// console.log(str);
+
+
+// let str = "school";
+
+// let replace_from = "o";
+// let replace_to = "i";
+
+// let index = 0;
+
+// let ans_str = "";
+
+// while(index < str.length) {
+
+//     if(str[index] == replace_from) {
+//         ans_str = ans_str + replace_to;
+//     } else {
+//         ans_str = ans_str + str[index];
+//     }
+
+//     index++;
+// }
+
+// console.log(ans_str);
+
+
+
+// ASCII CODE TO CHARACTER
+
+
+
+// let str = String.fromCharCode(75);
+
+// let ascii_code = "D";
+
+// console.log(ascii_code.charCodeAt());
+// console.log(str);
+
+// Q.  convert all lower case characters in upper case
+
+
+for(let index = 0; index < 10; index++) {
+    console.log(index);
+}
