92697e83d7fc8ce3f4cfe50f8d4b46c11e0fdb31
neerajnanvani <neerajnanvaniwork@gmail.com>
2024-02-23 21:50:08 +0530

    Array class 2 done

--- JS_week_6/script.js
+++ JS_week_6/script.js
@@ -22,15 +22,69 @@
 
 // Find how many student got pass in the class
 
-let arr = [95, 25, 54, 17, 62, 45, 75, 23, 9, 48, 72, 96, 58, 96, 75, 27, 33, 76, 100, 21, 45, 61, 12, 34, 56, 21, 65, 87];
+// let arr = [95, 25, 54, 17, 62, 45, 75, 23, 9, 48, 72, 96, 58, 96, 75, 27, 33, 76, 100, 21, 45, 61, 12, 34, 56, 21, 65, 87];
 
-let pass_students_count = 0;
+// let pass_students_count = 0;
 
-for(let index = 0; index < arr.length; index++) {
-    if(arr[index] > 33) {
-        pass_students_count++;
-    }
-}
+// for(let index = 0; index < arr.length; index++) {
+//     if(arr[index] > 33) {
+//         pass_students_count++;
+//     }
+// }
 
-// console.log("Out of", arr.length, "students", pass_students_count, "students got passed");
-console.log(`Out of ${arr.length} students ${pass_students_count} students got passed`);
+// // console.log("Out of", arr.length, "students", pass_students_count, "students got passed");
+// console.log(`Out of ${arr.length} students ${pass_students_count} students got passed`);
+
+let superHeros = ["Spiderman", "Superman", "Batman"];
+
+// console.log(superHeros[3], superHeros);
+
+console.log(superHeros);
+
+// superHeros.push("Ironman");
+
+// console.log(superHeros);
+
+// superHeros.pop();
+
+// console.log(superHeros);
+
+// superHeros.unshift("Hulk");
+
+// console.log(superHeros);
+
+// superHeros.shift();
+// console.log(superHeros);
+
+// superHeros.shift();
+// console.log(superHeros);
+
+// superHeros.shift();
+// console.log(superHeros);
+
+
+// superHeros.shift();
+// console.log(superHeros);
+
+// superHeros[4] = "Captain Marvel";
+
+// console.log(superHeros);
+
+superHeros.push("caption america", "hulk");
+
+console.log(superHeros);
+
+// console.log(superHeros.includes("Shaktiman"));
+
+// console.log(superHeros.includes("hulk"));
+
+console.log();
+
+
+let sabji_one = ["Bhindi", "Tamatar"];
+
+let sabji_two = ["AAloo", "Pyaaz"];
+
+sabji_one = sabji_one.concat(sabji_two);
+
+console.log(sabji_one.toString());
