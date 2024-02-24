d12ccd30f3f9679910224a60068cc04140bb68cf
neerajnanvani <neerajnanvaniwork@gmail.com>
2024-02-24 22:00:04 +0530

    Array class 3 done

--- JS_week_6/script.js
+++ JS_week_6/script.js
@@ -35,11 +35,11 @@
 // // console.log("Out of", arr.length, "students", pass_students_count, "students got passed");
 // console.log(`Out of ${arr.length} students ${pass_students_count} students got passed`);
 
-let superHeros = ["Spiderman", "Superman", "Batman"];
+// let superHeros = ["Spiderman", "Superman", "Batman"];
 
 // console.log(superHeros[3], superHeros);
 
-console.log(superHeros);
+// console.log(superHeros);
 
 // superHeros.push("Ironman");
 
@@ -70,21 +70,141 @@ console.log(superHeros);
 
 // console.log(superHeros);
 
-superHeros.push("caption america", "hulk");
+// superHeros.push("caption america", "hulk");
 
-console.log(superHeros);
+// console.log(superHeros);
 
 // console.log(superHeros.includes("Shaktiman"));
 
 // console.log(superHeros.includes("hulk"));
 
-console.log();
+// console.log();
 
 
-let sabji_one = ["Bhindi", "Tamatar"];
+// let sabji_one = ["Bhindi", "Tamatar"];
 
-let sabji_two = ["AAloo", "Pyaaz"];
+// let sabji_two = ["AAloo", "Pyaaz"];
 
-sabji_one = sabji_one.concat(sabji_two);
+// sabji_one = sabji_one.concat(sabji_two);
 
-console.log(sabji_one.toString());
+// console.log(sabji_one.toString());
+
+// let test = ( 3 * 4 );
+
+// let arr = [1, 2, 3];
+
+// console.log("a", arr);
+
+// let push_value = arr.push(4);
+
+// console.log("push",push_value);
+
+// console.log('b', arr);
+
+
+// let arr = [1, 2, 3];
+
+// console.log("a", arr);
+
+// let push_value = arr.pop();
+
+// console.log("pop",push_value);
+
+// console.log('b', arr);
+
+// let arr = [1, 2, 3];
+
+// console.log("a", arr);
+
+// let push_value = arr.unshift(4);
+
+// console.log("unshift",push_value);
+
+// console.log('b', arr);
+
+
+
+// let arr = [1, 2, 3];
+
+// console.log("a", arr);
+
+// let push_value = arr.shift();
+
+// console.log("shift",push_value);
+
+// console.log('b', arr);
+
+
+// let arr = [1, 2, 3];
+
+// console.log("a", arr);
+
+// let includes = arr.includes(4);
+
+// console.log("inc",includes);
+
+// console.log('b', arr);
+
+
+// let arr = [1, 2, 3];
+
+// console.log("a", arr);
+
+// let push_value = arr.toString();
+
+// console.log("str",push_value);
+
+// console.log('b', arr);
+
+// let arr = [1, 2, 3];
+
+// console.log("a", arr);
+
+// let push_value = arr.join("     ");
+
+// console.log("join",push_value);
+
+// console.log('b', arr);
+
+
+// let arr = [1, 2, 3];
+
+// console.log("a", arr);
+
+// let push_value = arr.indexOf(3);
+
+// console.log("indexOf",push_value);
+
+// console.log('b', arr);
+
+// let arr = [1, 2, 3, 4, 5];
+
+// console.log("a", arr);
+
+// let push_value = arr.reverse();
+
+// console.log("reverse",push_value);
+
+// console.log('b', arr);
+
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+
+// console.log("a", arr);
+
+// let push_value = arr.slice(2, 6);
+
+// console.log("slice",push_value);
+
+// console.log('b', arr);
+
+
+let arr = [1, 2, 3, 4,5, 6, 7];
+
+console.log("a", arr);
+
+let push_value = arr.splice(2, 6);
+
+console.log("splice",push_value);
+
+console.log('b', arr);
