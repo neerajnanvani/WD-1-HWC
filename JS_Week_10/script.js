0529462da037105ca0ecc241cf3439a32987cccd
neerajnanvani <neerajnanvaniwork@gmail.com>
2024-04-17 21:56:00 +0530

    dom day 1 done

---
+++ JS_Week_10/script.js
@@ -0,0 +1,16 @@
+// console.log("Test");
+
+
+// let para = document.getElementById("para_ne");
+// console.log(para);
+
+// document.getElementById("para_one").innerHTML = "THis is change html";
+
+// let head = document.getElementsByClassName("test_heading");
+
+// console.log(head);
+
+
+let inputVal = document.getElementById('test_input');
+
+console.log(inputVal.value);
