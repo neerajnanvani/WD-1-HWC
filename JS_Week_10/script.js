0b6454654552433a783a733a6f3ac2665dd46677
neerajnanvani <neerajnanvaniwork@gmail.com>
2024-04-22 21:39:31 +0530

    Add event listner done

--- JS_Week_10/script.js
+++ JS_Week_10/script.js
@@ -9,8 +9,24 @@
 // let head = document.getElementsByClassName("test_heading");
 
 // console.log(head);
-
-
 let inputVal = document.getElementById('test_input');
 
-console.log(inputVal.value);
+let test_btn = document.getElementById("test_btn");
+
+function listenInput(event) {
+    console.log(event.target.value);
+    if(event.target.value >= 18) {
+        test_btn.disabled = false;
+    } else {
+        test_btn.disabled = true;
+    }
+}
+
+inputVal.addEventListener("input", listenInput);
+
+function listenBtn(event) {
+    console.log(inputVal.value);
+}
+
+
+test_btn.addEventListener("click", listenBtn);
