4f70b6dfce28bb0db84c9c37306731bfb097fc6b
neerajnanvani <neerajnanvaniwork@gmail.com>
2024-04-26 21:55:32 +0530

    query selector and text selector done

--- JS_Week_10/script.js
+++ JS_Week_10/script.js
@@ -9,24 +9,50 @@
 // let head = document.getElementsByClassName("test_heading");
 
 // console.log(head);
-let inputVal = document.getElementById('test_input');
+// let inputVal = document.getElementById('test_input');
 
-let test_btn = document.getElementById("test_btn");
+// let test_btn = document.getElementById("test_btn");
 
-function listenInput(event) {
-    console.log(event.target.value);
-    if(event.target.value >= 18) {
-        test_btn.disabled = false;
+// function listenInput(event) {
+//     console.log(event.target.value);
+//     if(event.target.value >= 18) {
+//         test_btn.disabled = false;
+//     } else {
+//         test_btn.disabled = true;
+//     }
+// }
+
+// inputVal.addEventListener("input", listenInput);
+
+// function listenBtn(event) {
+//     console.log(inputVal.value);
+// }
+
+
+// test_btn.addEventListener("click", listenBtn);
+
+
+// let test_it = document.querySelector("p");
+// console.log(test_it);
+
+
+let btn_selector = document.getElementById("showbtn");
+
+let text_selector = document.getElementById("the_text");
+
+let text_to_show = false;
+
+function toggleText() {
+
+    if(text_to_show) {
+        text_selector.style.display = "none";
+        text_to_show = false;
+        btn_selector.innerHTML = "Show Text";
     } else {
-        test_btn.disabled = true;
+        text_selector.style.display = "block";
+        text_to_show = true;
+        btn_selector.innerHTML = "Hide Text";
     }
 }
 
-inputVal.addEventListener("input", listenInput);
-
-function listenBtn(event) {
-    console.log(inputVal.value);
-}
-
-
-test_btn.addEventListener("click", listenBtn);
+btn_selector.addEventListener("click", toggleText);
