821925a675a2eed3c19ab4d8bf756c04f6b5472f
neerajnanvani <neerajnanvaniwork@gmail.com>
2024-05-01 21:46:15 +0530

    Form day 2 done

--- JS_WEEK_11/script.js
+++ JS_WEEK_11/script.js
@@ -1,8 +1,21 @@
-function alertMessage(message) {
-    alert("Testing");
-}
-
+let input_selectors = document.getElementsByClassName("form_input");
 
 let submit_btn_selector = document.getElementById("submit_btn");
 
-submit_btn_selector.addEventListener("click", alertMessage);
+function alertMessage(message) {
+    alert(message);
+}
+
+function testInputFields() {
+    for(let index = 0; index < input_selectors.length; index++) {
+        if(input_selectors[index].value == '') {
+            alertMessage("Please fill all the fields");
+            return;
+        }
+    }
+
+    alertMessage("Form Validation Done")
+}
+
+submit_btn_selector.addEventListener("click", testInputFields);
+
