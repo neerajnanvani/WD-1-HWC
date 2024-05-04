275c28cbf9a55baec7b05fdf7ebc842ebf769851
neerajnanvani <neerajnanvaniwork@gmail.com>
2024-05-04 21:59:28 +0530

    Form day 3 done

--- JS_WEEK_11/script.js
+++ JS_WEEK_11/script.js
@@ -2,6 +2,10 @@ let input_selectors = document.getElementsByClassName("form_input");
 
 let submit_btn_selector = document.getElementById("submit_btn");
 
+let clear_btn_selector = document.getElementById("clear_btn");
+
+let age_input_selector = document.getElementById("age");
+
 function alertMessage(message) {
     alert(message);
 }
@@ -14,8 +18,26 @@ function testInputFields() {
         }
     }
 
+    if(age_input_selector.value < 0) {
+        alertMessage("Please fill correct age");
+        return;
+    }
+
+    if(age_input_selector.value < 18) {
+        alertMessage("Please fill age above or equal 18");
+        return;
+    }
+
     alertMessage("Form Validation Done")
 }
 
+function clearAllValues() {
+    for(let index = 0; index < input_selectors.length; index++) {
+        input_selectors[index].value = '';
+    }
+}
+
 submit_btn_selector.addEventListener("click", testInputFields);
+
+clear_btn_selector.addEventListener("click", clearAllValues);
 
